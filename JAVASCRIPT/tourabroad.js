6f481edeba243564d05c90e34ddf312751e6f89d
ktpmduy <141805318+ktpmduy@users.noreply.github.com>
2024-03-27 00:39:17 +0700

    duy

---
+++ JAVASCRIPT/tourabroad.js
@@ -0,0 +1,66 @@
+const slides = document.getElementsByClassName('Slide')
+const dots = document.querySelectorAll('.function_btn span')
+const pre = document.querySelector('#pre')
+const next = document.querySelector('#next')
+
+var current = 0
+
+function updateSlide(n) {
+    slides[current].classList.remove('active')
+    dots[current].style.background = '#1d262f'
+    dots[current].style.color = '#fff'
+
+    current = n
+
+    dots[current].style.background = 'rgba(47, 50, 47, 0.479)'
+    dots[current].style.color = 'black'
+    slides[current].classList.add('active')
+
+    if (!(current > 0)) {
+        pre.style.display = 'none'
+        next.style.display = 'unset'
+    }
+
+    if (!(current + 1 < slides.length)) {
+        pre.style.display = 'unset'
+        next.style.display = 'none'
+    }
+
+    if(((current > 0) && (current + 1 < slides.length))){
+        pre.style.display = 'unset'
+        next.style.display = 'unset'
+    }
+}
+
+function plus(n) {
+    slides[current].classList.remove('active')
+    dots[current].style.background = '#1d262f'
+    dots[current].style.color = '#fff'
+    
+    current = current + n
+
+    if (!(current < slides.length))
+        current = 0
+
+    if (current < 0)
+        current = slides.length - 1
+
+    dots[current].style.background = 'rgba(47, 50, 47, 0.479)'
+    dots[current].style.color = 'black'
+    slides[current].classList.add('active')
+
+    if (!(current > 0)) {
+        pre.style.display = 'none'
+        next.style.display = 'unset'
+    }
+
+    if (!(current + 1 < slides.length)) {
+        pre.style.display = 'unset'
+        next.style.display = 'none'
+    }
+
+    if(((current > 0) && (current + 1 < slides.length))){
+        pre.style.display = 'unset'
+        next.style.display = 'unset'
+    }
+}
