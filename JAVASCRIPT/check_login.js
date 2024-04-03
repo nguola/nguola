00908a1d86e7b570a1a75acbd55748233c7f72d0
Trịnh Quang Toàn Hảo <142070040+nguola@users.noreply.github.com>
2024-04-03 19:30:19 +0700

    th

--- JAVASCRIPT/check_login.js
+++ JAVASCRIPT/check_login.js
@@ -2,6 +2,7 @@ let sts = localStorage.getItem('status')
 
 let login_display = document.querySelectorAll('.login_signin')
 let account_display = document.querySelectorAll('.account')
+let user_current = localStorage.getItem('user_current')
 
 let logout_btn = document.querySelector('#logout')
 
@@ -10,11 +11,11 @@ window.onload = function(){
 }
 
 function check_login(){
-    console.log(sts)
-
     if (sts !== null) {
         login_display[0].style.display = 'none'
         account_display[0].style.display = 'block'
+        let user_name = document.querySelector('#user_name')
+        user_name.innerHTML += user_current
     }
     else {
         login_display[0].style.display = 'flex'
@@ -22,6 +23,9 @@ function check_login(){
     }
     
     logout_btn.addEventListener('click', ()=>{
+        let account = JSON.parse(localStorage.getItem(user_current))
+        account.status = 0;
+        localStorage.setItem(user_current, JSON.stringify(account));
         localStorage.removeItem('status')
         login_display[0].style.display = 'flex'
         account_display[0].style.display = 'none'
