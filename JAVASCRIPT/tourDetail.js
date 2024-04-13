158e08aba71e5ef61b8cec738c686490ee12f25c
Hungzazed <119870697+Hungzazed@users.noreply.github.com>
2024-04-13 17:58:38 +0700

    NPH

--- JAVASCRIPT/tourDetail.js
+++ JAVASCRIPT/tourDetail.js
@@ -16,3 +16,51 @@ function showSlider() {
     carouselDom.classList.remove('next1');
   }, timeRunning);
 }
+
+
+function goToThanhToan() {
+  let check_in = $('#check_in');
+  let check_out = $('#check_out');
+  let numPeople = $('#NumberPeople');
+
+  if (!check_in.val()) {
+    alert('Chưa nhập ngày check_in');
+    return false;
+  }
+  if (!check_out.val()) {
+    alert('Chưa nhập ngày check out');
+    return false;
+  }
+
+  if (!numPeople.val()) {
+    alert('Chưa nhập số lượng người');
+    return false;
+  }
+
+  let check_in_date = new Date(check_in.val());
+  let check_out_date = new Date(check_out.val());
+
+  if (check_in_date < new Date()) {
+    alert('Ngày check in phải sau ngày hiện tại');
+    return false;
+  }
+  if (check_in_date > check_out_date) {
+    alert('Ngày check in phải trước ngày check out');
+    return false;
+  }
+
+  let price = $('.price').eq(0).text();
+  let name = $('.header h1').eq(0).text();
+  let img = $('.list .item img').eq(0).attr('src');
+  let currentURL = window.location.href;
+  let url = currentURL.substring(currentURL.lastIndexOf("/") + 1);
+  let pre_tour = {
+    img,
+    name,
+    price,
+    url
+  }
+  localStorage.setItem('pre_tour', JSON.stringify(pre_tour));
+
+  window.location.href = 'payment.html';
+}
